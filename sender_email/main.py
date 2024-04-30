4e8ad3cc9dd95c252209a33b89ce8712c9129424
Jaime Herran <jaimehmol@yahoo.com>
2024-04-29 19:53:28 -0500

    Fix email sender dummy service.

--- sender_email/main.py
+++ sender_email/main.py
@@ -3,22 +3,27 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-SENDER_EMAIL = "herranmolano@gmail.com"
-PASSWORD = "Mephisto" #os.environ["EMAIL_PASSWORD"]
+from alert_notifications_api.models.notification_dto import NotificationDTO
+
+SENDER_EMAIL = "usertest@gmail.com"
+SUBJECT = "Promo from your real state company :)"
+PASSWORD = "Dante Aligerry" #os.environ["EMAIL_PASSWORD"]
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587 # Standard SMTP port
 
 
-def send_email(receiver_email: str, subject: str, message: str):
+def send_email(email_details: NotificationDTO):
 
     email_message = MIMEMultipart()
     email_message['From'] = SENDER_EMAIL
-    email_message['To'] = receiver_email
-    email_message['Subject'] = subject
-    email_message.attach(MIMEText(message, 'plain'))
+    email_message['To'] = email_details.customer_email
+    email_message['Subject'] = SUBJECT
+    email_message.attach(MIMEText(email_details.offer_message, 'plain'))
 
-    print(f"Email sent to {receiver_email}!")
-    return f"Email sent to {receiver_email}!"
+    print(f"Email sent to {email_details.customer_email}!")
+    return f"Email sent to {email_details.customer_email}!"
+
+
     # with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
     #     server.starttls()
     #     server.login(SENDER_EMAIL, PASSWORD)  
--- tests/unit/test_sender_email.py
+++ tests/unit/test_sender_email.py
@@ -1,10 +1,16 @@
+from alert_notifications_api.models.notification_dto import NotificationDTO
 from sender_email.main import send_email
 
 
 def test_send_email():
-    receiver_email = 'herranmolano@gmail.com'  # Cambia 'recipient@example.com' a la dirección de correo del destinatario
-    subject = 'Prueba de correo electrónico'
-    message = 'Hola, este es un mensaje de prueba enviado desde Python.'
+    """ Test the dummy email sent """
+    email_details = NotificationDTO(
+        customer_email="user_test@hotmail.com",
+        email=True,
+        customer_cellphone=3445556677,
+        sms=False,
+        offer_message="Test body email"
+    )
 
-    response = send_email(receiver_email, subject, message)
-    assert response == f"Email sent to {receiver_email}!"
+    response = send_email(email_details)
+    assert response == f"Email sent to {email_details.customer_email}!"
